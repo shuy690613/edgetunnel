bb54fae1bcd05058cb1571b4918d6ffd92518e9e
shuy690613 <shuy1152880304@Outlook.com>
2024-12-07 12:36:56 +0800

    Update 明文源码.js

--- 明文源码.js
+++ 明文源码.js
@@ -1,7 +1,7 @@
 
 import { connect } from 'cloudflare:sockets';
 
-let userID = 'f23d7a83-2ebc-41e3-8278-727fd45156b3';
+let userID = '';
 let proxyIP = '';
 let sub = '';
 let subConverter = 'SUBAPI.fxxk.dedyn.io';
